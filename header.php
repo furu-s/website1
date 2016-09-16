b05f7bea8863c7262ebc4de78cb45fb316c6ae85
furu-s <furu.s11@gmail.com>
2016-09-16 16:53:12 +0900

    ページ追加

---
+++ header.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <title>世界食料デーフェス</title>
+  <meta charset="utf-8">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.7&appId=1572630606326790";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
--- index.php
+++ index.php
@@ -1,1 +1,110 @@
-test
+<?php require_once("header.php"); ?>
+  <div class="container-fluid header">
+    <div class="row">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#gnavi">
+        <span class="sr-only">メニュー</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      </div>
+    <nav class="navbar navbar-light bg-faded">
+    <div id="gnavi" class="collapse navbar-collapse">
+    <ul class="nav text-center">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">ホーム</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">主催者について</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">協賛について</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">お問い合わせ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">アクセス</a>
+      </li>
+    </ul>
+    </div>
+    </nav>
+    </div>
+  </div>
+  <div class="container-fluid text-center main-slideshow-wrap">
+    <div class="row">
+      <h1><img src="images/top/logo.png" alt="" class="header-logo"></h1>
+    </div>
+    <div class="row main-slidershow">
+      <img src="images/top/slide-1.jpg" alt="" class="main-slideshow-item" width="90%">
+    </div>
+  </div>
+  <section class="section section-theme">
+  <div class="container text-center">
+    <div class="row">
+    <img src="images/top/theme.png" width="100%" alt="">
+     </div>
+  </div>
+  </section>
+
+  <section class="section section-news">
+    <div class="container">
+      <div class="row">
+        <h2 class="headline text-center">食料デーフェス 速報配信中</h2>
+        <div class="text-center">
+          <div class="fb-page" data-href="https://www.facebook.com/wfd.fes" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/wfd.fes" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/wfd.fes">世界食料デーフェスティバル実行委員会</a></blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section section-sponsor">
+    <div class="container">
+      <div class="row">
+        <h2 class="headline text-center">スポンサー企業紹介</h2>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%"></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+        <div class="col-md-3 col-xs-6 sponsor-item text-center">
+          <a href=""><img src="images/top/logo_city-yokohama.png" alt="" width="60%" ></a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section section-access">
+    <div class="container">
+      <div class="row text-center">
+        <h2 class="headline text-center">アクセスマップ</h2>
+        <p class="text-left access-desc">
+        ◯◯線◯番出口徒歩◯分<br>
+        ◯◯線◯番出口徒歩◯分<br>
+        ◯◯線◯番出口徒歩◯分<br>
+        ◯◯線◯番出口徒歩◯分
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.5115701528784!2d139.75775691495778!3d35.664403680198234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bc2624d7de5%3A0xa3e71571e3878398!2z5pel44OG44Os44K_44Ov44O8!5e0!3m2!1sja!2sjp!4v1473818699767" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </section>
+
+ <?php require_once("footer.php"); ?>
